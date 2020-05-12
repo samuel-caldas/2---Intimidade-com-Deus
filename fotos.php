181067ab5a0f9ba1099cedbccbca41a3560ad624
Samuel Caladas <samuel.caldas@gmail.com>
2020-05-12 14:58:28 -0300

    Initial commit

---
+++ fotos.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fotos - Intimidade com Deus</title>
+<link rel="icon" href="http://icons.iconarchive.com/icons/poundffffff/blox-folder/128/Music-Folder-icon.png" />
+<link href="style.css" rel="stylesheet" type="text/css">
+<link id="css" href=""	rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/windowsize.js"></script>
+<script>
+window.onload=function(){
+	document.getElementById("css").href="style.php?h="+WindowHeight()+"&w="+WindowWidth();
+};
+</script>
+</head>
+<body id="body" class="body">
+<div id="" class="pag">
+	<div id="" class="menu">
+    	<div class="centralize">
+          <a onClick="" href="index.html" class="menubtn" id="">In&iacute;cio</a>
+          <a onClick="" href="fotos.php" class="menubtnactive" id="">Fotos</a>
+          <a onClick="" href="blog.php" class="menubtn" id="">Blog</a>
+          <a onClick="" href="agenda.php" class="menubtn" id="">Agenda</a>
+          <a onClick="" href="produtos.php" class="menubtn" id="">Produtos</a>
+          <a onClick="" href="contato.html" class="menubtn" id="">Contato</a>
+		</div>
+  </div>
+    <div id="" class="title">Fotos</div>
+    <div id="" class="content">
+    	<div class="fotos" id="fotos"></div>
+    </div>
+  <div id="" class="botton">
+    	<a onClick="" href="#" class="fbook" id=""><strong>facebook</strong></a>
+        <a onClick="" href="#" class="twtter" id="">Twitter</a>
+  </div>
+</div>
+</body>
+</html>
---
+++ style.php
@@ -0,0 +1,5 @@
+<?php header('Content-type:text/css'); ?>
+@charset "utf-8";
+.pag{
+	min-height: <?php echo($_GET['h']-100) ?>px;
+}
